2a62c888bd20c35bc92c234026f8b5b6e8ab0364
uyutaka <yutaka.urakami@colorado.edu>
2023-07-16 07:33:58 +0000

    TestLetStatments (2.4 Parser's first steps: parsing let statements)

---
+++ parser/parser.go
@@ -0,0 +1,34 @@
+package parser
+
+import (
+	"github.com/uyutaka/interpreter-in-golang/ast"
+
+	"github.com/uyutaka/interpreter-in-golang/lexer"
+	"github.com/uyutaka/interpreter-in-golang/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	// Read two tokens, so curToken and peekToken are both set
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
